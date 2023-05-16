6acf09e8d89ed72323370c087fef0acdaacda0de
Harshil Shah <atlantismont@gmail.com>
2023-05-15 22:08:11 -0400

    added base code for setting up

--- server/src/main/java/com/example/Fit4Lyfe/controllers/UserController.java
+++ server/src/main/java/com/example/Fit4Lyfe/controllers/UserController.java
@@ -1,4 +1,24 @@
 package com.example.Fit4Lyfe.controllers;
 
+import com.example.Fit4Lyfe.models.User;
+import com.example.Fit4Lyfe.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("")
+    public List<User> getAllUsers() {
+        List<User> users = userRepository.findAll();
+
+        return users;
+    }
 }
